b7c63b5084325917b8a03638323db04cc2e493b8
Arthur Valadares <arthur.c.valadares@gmail.com>
2023-06-09 10:07:22 -0300

    Implementação do SharedPreferences para o tema.

--- lib/providers/theme_manager.dart
+++ lib/providers/theme_manager.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeManager extends ChangeNotifier {
-  ThemeMode? state = ThemeMode.system;
+  ThemeMode? state;
 
-  void switchTo(ThemeMode? arg) {
+  ThemeManager({this.state}) {
+    _initialize();
+  }
+
+  void _initialize() {
+    SharedPreferences.getInstance().then((value) {
+      final int? themeMode = value.getInt('themeMode');
+      if (themeMode == 1) {
+        state = ThemeMode.light;
+      } else if (themeMode == 2) {
+        state = ThemeMode.dark;
+      } else if (themeMode == 3) {
+        state = ThemeMode.system;
+      }
+    });
+  }
+
+  void switchTo(ThemeMode? arg) async {
     state = arg;
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    int value = 0;
+    if (state == ThemeMode.light) {
+      value = 1;
+    } else if (state == ThemeMode.dark) {
+      value = 2;
+    } else if (state == ThemeMode.system) {
+      value = 3;
+    }
+    pref.setInt('themeMode', value);
     notifyListeners();
   }
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:subnet_mask_calculator/components/expansible_card.dart';
-import 'package:subnet_mask_calculator/components/list_tile_with_switch.dart';
 import 'package:subnet_mask_calculator/components/my_navigation_bar.dart';
 import 'package:subnet_mask_calculator/providers/theme_manager.dart';
 import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -13,60 +13,202 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  void Function(bool)? _switchDarkThemeEnable(BuildContext context) {
-    if (Provider.of<ThemeManager>(context).state != ThemeMode.system) {
-      return (bool value) {
-        if (value) {
-          Provider.of<ThemeManager>(context, listen: false)
-              .switchTo(ThemeMode.dark);
-        } else {
-          Provider.of<ThemeManager>(context, listen: false)
-              .switchTo(ThemeMode.light);
-        }
-      };
+  double radius = 50;
+
+  static const Tuple2<double, double> pairNotScaled = Tuple2(1, 1);
+  static const Tuple2<double, double> pairScaled = Tuple2(1.2, 0.8);
+
+  static const Duration scaleTransitionDuaration = Duration(milliseconds: 400);
+
+  late Tuple2<double, double> whiteOptionScale;
+  late Tuple2<double, double> blackOptionScale;
+  late Tuple2<double, double> systemOptionScale;
+
+  void _scale(ThemeMode value, Tuple2<double, double> variable) {
+    whiteOptionScale = blackOptionScale = systemOptionScale = pairNotScaled;
+    if (value == Provider.of<ThemeManager>(context, listen: false).state) {
+      setState(() {
+        variable = pairScaled;
+      });
+    } else {
+      setState(() {
+        variable = pairNotScaled;
+      });
     }
-    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    whiteOptionScale =
+        blackOptionScale = systemOptionScale = const Tuple2(1, 1);
   }
 
   @override
   Widget build(BuildContext context) {
+    ThemeMode? state = Provider.of<ThemeManager>(context, listen: false).state;
+    if (state != null) {
+      if (state == ThemeMode.system) {
+        systemOptionScale = pairScaled;
+      } else if (state == ThemeMode.dark) {
+        blackOptionScale = pairScaled;
+      } else if (state == ThemeMode.light) {
+        whiteOptionScale = pairScaled;
+      }
+    } else {
+      if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+        blackOptionScale = pairScaled;
+      } else {
+        whiteOptionScale = pairScaled;
+      }
+    }
+
     return Scaffold(
       extendBody: true,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: const Text("Configurações"),
       ),
-      body: ListView.builder(
+      body: ListView(
           physics: const BouncingScrollPhysics(
               parent: AlwaysScrollableScrollPhysics()),
           padding:
               const EdgeInsets.only(left: 20, bottom: 250, right: 20, top: 100),
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return Consumer<ThemeManager>(builder: (context, value, child) {
-                return ExpansibleCard(
-                  title: "Opções de Tema",
+          children: [
+            ExpansibleCard(title: "Opções de Tema", children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ListTitleWithSwitch(
-                        title: const Text("Tema do sistema."),
-                        switchInitialValue: value.state == ThemeMode.system,
-                        onSwitchChange: (switchValue) {
-                          if (switchValue) {
-                            value.switchTo(ThemeMode.system);
-                          } else {
-                            value.switchTo(null);
-                          }
-                        }),
-                    ListTitleWithSwitch(
-                        title: const Text("Tema escuro"),
-                        switchInitialValue: value.state == ThemeMode.light,
-                        onSwitchChange: _switchDarkThemeEnable(context))
+                    InkWell(
+                      onTap: () {
+                        Provider.of<ThemeManager>(context, listen: false)
+                            .switchTo(ThemeMode.light);
+                        _scale(ThemeMode.light, whiteOptionScale);
+                      },
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 3),
+                                child: AnimatedScale(
+                                  duration: scaleTransitionDuaration,
+                                  scale: whiteOptionScale.item1,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(color: Colors.grey)),
+                                    child: AnimatedScale(
+                                      duration: scaleTransitionDuaration,
+                                      scale: whiteOptionScale.item2,
+                                      child: Container(
+                                        height: radius,
+                                        width: radius,
+                                        decoration: const BoxDecoration(
+                                          color: Colors.white,
+                                          shape: BoxShape.circle,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const Text("Claro")
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Provider.of<ThemeManager>(context, listen: false)
+                            .switchTo(ThemeMode.dark);
+                        _scale(ThemeMode.dark, blackOptionScale);
+                      },
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 3),
+                                child: AnimatedScale(
+                                  scale: blackOptionScale.item1,
+                                  duration: const Duration(milliseconds: 400),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(color: Colors.grey)),
+                                    child: AnimatedScale(
+                                      duration: scaleTransitionDuaration,
+                                      scale: blackOptionScale.item2,
+                                      child: Container(
+                                        height: radius,
+                                        width: radius,
+                                        decoration: const BoxDecoration(
+                                          color: Colors.black,
+                                          shape: BoxShape.circle,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const Text("Escuro")
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Provider.of<ThemeManager>(context, listen: false)
+                            .switchTo(ThemeMode.system);
+                        _scale(ThemeMode.system, systemOptionScale);
+                      },
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 3),
+                                child: AnimatedScale(
+                                  duration: scaleTransitionDuaration,
+                                  scale: systemOptionScale.item1,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(color: Colors.grey)),
+                                    child: AnimatedScale(
+                                      duration: scaleTransitionDuaration,
+                                      scale: systemOptionScale.item2,
+                                      child: Container(
+                                        height: radius,
+                                        width: radius,
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(0.1),
+                                          shape: BoxShape.circle,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const Text("Sistema")
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
                   ],
-                );
-              });
-            }
-            return null;
-          }),
+                ),
+              )
+            ]),
+          ]),
       bottomNavigationBar: const MyNavigationBar(),
     );
   }
